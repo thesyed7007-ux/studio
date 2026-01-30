4bca2ac791d2376c9838c72721d235c0b500fe19
Syed Luqman <thesyed7007@gmail.com>
2026-01-30 15:29:27 +0000

    For fire base free hosting add this to code

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-8612578263-b0651",
-  "appId": "1:698295987721:web:e5ad285a84116d6fe112d8",
-  "apiKey": "AIzaSyBupA-9j-maVpg_ODdwNsRT4YN3yQoHG2Q",
+  "appId": "1:698295987721:web:4774f38c9f49b5c9e112d8",
+  "apiKey": "AIzaSyBupA-9j-maVpg_ODdwNsRT4YN3YQoHG2Q",
   "authDomain": "studio-8612578263-b0651.firebaseapp.com",
-  "measurementId": "",
+  "storageBucket": "studio-8612578263-b0651.firebasestorage.app",
   "messagingSenderId": "698295987721"
 };
